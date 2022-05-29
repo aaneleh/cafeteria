9a26d8ee5bb894a61a6b1b68f15b900bb5243870
aaneleh <helenakurzgon@gmail.com>
2022-05-28 21:59:58 -0300

    home estática

---
+++ script/search.js
@@ -0,0 +1,7 @@
+function toggleSearch() {
+    const searchBar = document.getElementById('search-bar');
+    searchBar.classList.toggle('active');
+}
+function pesquisar() {
+    alert('pesquisa feita');
+}
