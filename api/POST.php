e181aae0ad364cb8d50cce71df07b587221fb5e0
aaneleh <helenakurzgon@gmail.com>
2022-06-12 01:44:30 -0300

    adicionar novo produto

--- api/POST.php
+++ api/POST.php
@@ -1,6 +1,12 @@
 <?php
     session_start();
 
+
+    $jsonProdutos = file_get_contents("../produtos.json");
+    $dadosProdutos = json_decode($jsonProdutos, true);
+
+    $page = "";
+
     $action = $_POST['action'];
 
     switch ($action){
@@ -10,11 +16,33 @@
                 $_SESSION['logon'] = true;
                 $page = 'pedidos';
             }
+            break;
+        case 'adicionar':
+            $page = "adicionar&status=invalid";
+            if(isset($_SESSION['logon']) and $_SESSION['logon'] == 1){
+                $novoProduto = new stdClass();
+
+                if($_POST['nome'] != "" and $_POST['preco'] != "") {
+                    $novoProduto->nome= $_POST['nome'];
+                    //$novoProduto->arquivo = $_POST[''];
+                    $_POST['descricao'] == "" ? $novoProduto->descricao = "sem descrição" : $novoProduto->descricao = $_POST['descricao'];
+                    $novoProduto->preco = $_POST['preco'];
+                    $novoProduto->categoria = $_POST['categoria'];
+
+                    array_push($dadosProdutos, $novoProduto);
+                    if(!file_put_contents("../produtos.json", json_encode($dadosProdutos, JSON_PRETTY_PRINT), LOCK_EX)){
+                        echo("ERRO SALVANDO O ARQUIVO");
+                    } else {
+                        echo("SUCESSO");
+                        $page = "produtos";
+                    }
+                }
+                var_dump($dadosProdutos);
+            }
 
             break;
     }
 
     header('Location: index.php?pag='.$page); 
     exit;
-
 ?>
--- api/adicionar.php
+++ api/adicionar.php
@@ -1,13 +1,36 @@
-<form action="" method="POST">
+<form action="POST.php" method="POST">
     <input type="hidden" name="action" value="adicionar">
 
     <h1>Novo produto</h1>
+
+    <?php 
+        if(isset($_GET['status']) and $_GET['status'] == 'invalid'){
+            echo 'Nome ou preço inválido';
+        }
+
+    ?>
+
     <h2>Nome:</h2>
     <input type="text" name="nome">
+
     <h2>Descricao:</h2>
     <input type="text" name="descricao">
+
     <h2>Preço:</h2>
-    <input type="number" name="preco">
+    <input type="number" name="preco" step=".01">
+
+    <h2>Categoria:</h2>
+    <input list="categorias" name="categoria">
+    <datalist id="categorias">
+        <?php
+            echo '<option value="doces">';
+            echo '<option value="bebidas">';
+        ?>
+    </datalist>
+
     <h2>Imagem:</h2>
     <input type="file" name="imagem">
+
+    <br><br>
+    <input type="submit" class="buy-button" value="Adicionar">
 </form>
